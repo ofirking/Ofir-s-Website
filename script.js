37061bb99506857cb2f0531b716663c8e791891d
ofirking <oriabiton123456@gmail.com>
2024-12-27 08:24:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function buyNow(productName, price) {
-    alert(`בחרת לקנות "${productName}" במחיר ${price} ש"ח. מעביר לתשלום...`);
-    window.location.href = `https://www.paypal.com/paypalme/YOUR_PAYPAL_LINK/${price}`;
+// פונקציה לתשלום בפייפאל
+function payWithPayPal() {
+    window.location.href = "https://www.paypal.com/checkoutnow"; // קישור לדף תשלום PayPal שלך
+}
+
+// פונקציה לתשלום בביט
+function payWithBit() {
+    alert("לשלם בביט יש לשלוח לכתובת הטלפון: 050-1234567");
+    // כאן ניתן להוסיף הפנייה לאפליקציה של ביט, אם יש תמיכה
+}
+
+// פונקציה לתשלום בפייבוקס
+function payWithPayBox() {
+    alert("לשלם בפייבוקס יש לשלוח לכתובת: https://payboxapp.page.link/example");
+    // כאן ניתן להוסיף לינק מותאם אישית
 }
